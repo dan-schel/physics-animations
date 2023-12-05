c00c487f00039648ea1497db459c37848bbe302b
schel-d <schel.d.dev@gmail.com>
2023-12-05 14:33:49 +1100

    setup 404 page

---
+++ app/(index)/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <p>Home page</p>
+    </main>
+  );
+}
