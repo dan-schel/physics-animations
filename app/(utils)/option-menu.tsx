ea2a6bc5b0bdfa34bc035ef780c111190692fa8f
schel-d <schel.d.dev@gmail.com>
2023-12-07 11:19:26 +1100

    expandable option menu

--- app/(utils)/option-menu.tsx
+++ app/(utils)/option-menu.tsx
@@ -5,7 +5,7 @@ import {
   AnimationOptions,
 } from "@/data/options";
 import { AnimationType } from "@/data/animation";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 
 export default function OptionMenu({
   animation,
@@ -18,17 +18,34 @@ export default function OptionMenu({
     SetStateAction<AnimationOptionValues<AnimationOptions>>
   >;
 }) {
+  const [expanded, setExpanded] = useState(true);
+
+  function handleExpandButtonClick() {
+    setExpanded((currentValue) => !currentValue);
+  }
+
   return (
-    <div className={styles.options}>
-      <p>Customize animation</p>
-      {animation.options.definitions.map((o) => (
-        <OptionInput
-          key={o.id}
-          definition={o}
-          value={optionValues.get(o.id)}
-          onChange={(value) => setOptionValues(optionValues.with(o.id, value))}
-        ></OptionInput>
-      ))}
+    <div className={styles.menu}>
+      <button className={styles.expandButton} onClick={handleExpandButtonClick}>
+        <p>Customize animation</p>
+        <ChevronIcon
+          className={expanded ? styles.down : styles.up}
+        ></ChevronIcon>
+      </button>
+      {expanded && (
+        <div className={styles.options}>
+          {animation.options.definitions.map((o) => (
+            <OptionInput
+              key={o.id}
+              definition={o}
+              value={optionValues.get(o.id)}
+              onChange={(value) =>
+                setOptionValues(optionValues.with(o.id, value))
+              }
+            ></OptionInput>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
@@ -79,5 +96,22 @@ function BooleanOptionInput({
         </div>
       </div>
     </label>
+  );
+}
+
+function ChevronIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      className={`icon ${className ?? ""}`}
+    >
+      <path
+        fill="currentColor"
+        d="M15.54 11.29L9.88 5.64a1 1 0 0 0-1.42 0a1 1 0 0 0 0 1.41l4.95 5L8.46 17a1 1 0 0 0 0 1.41a1 1 0 0 0 .71.3a1 1 0 0 0 .71-.3l5.66-5.65a1 1 0 0 0 0-1.47Z"
+      />
+    </svg>
   );
 }
