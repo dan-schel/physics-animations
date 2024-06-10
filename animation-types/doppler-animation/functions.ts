b038a194b069c8829fd300a0805f9593dd391141
dan-schel <me@danschellekens.com>
2024-06-10 17:38:39 +1000

    start from left

--- animation-types/doppler-animation/functions.ts
+++ animation-types/doppler-animation/functions.ts
@@ -2,7 +2,7 @@ export type PositionFunction = (time: number) => { x: number; y: number };
 
 export function sine(period: number, width: number): PositionFunction {
   return (time: number) => ({
-    x: Math.sin((time / period) * Math.PI * 2) * width,
+    x: -Math.cos((time / period) * Math.PI * 2) * width,
     y: 0,
   });
 }
